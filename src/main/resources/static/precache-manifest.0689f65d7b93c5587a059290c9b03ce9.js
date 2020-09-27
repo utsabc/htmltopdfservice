ed0f87d52a302b4e3d5244b5662948aaf486bbd2
Utsab Chowdhury <utsab.c97@gmail.com>
2020-09-27 19:08:01 +0530

    Adding React frontend build resources

---
+++ src/main/resources/static/precache-manifest.0689f65d7b93c5587a059290c9b03ce9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "071886df33644d69230e09d0884dc896",
+    "url": "/index.html"
+  },
+  {
+    "revision": "756bd487dc63a7f7bd68",
+    "url": "/static/css/main.fc1dfdbe.chunk.css"
+  },
+  {
+    "revision": "6290c31294d516db85f7",
+    "url": "/static/js/2.3fd7e810.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3fd7e810.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "756bd487dc63a7f7bd68",
+    "url": "/static/js/main.b9a3797b.chunk.js"
+  },
+  {
+    "revision": "01e19166fa36e197829a",
+    "url": "/static/js/runtime-main.eea7577e.js"
+  }
+]);
